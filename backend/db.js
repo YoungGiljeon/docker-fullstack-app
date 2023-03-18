912a01d964587cc073cf43c539cf1aefa41efefb
YounGil Jeon <cooljyg@gmail.com>
2023-03-18 21:49:03 +0900

    git delete

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql");
-const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: 'mysql',
-    user: 'root',
-    password: 'dudrlf00!',
-    database: 'myapp'
-});
-exports.pool = pool;
